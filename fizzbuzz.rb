c9bf594091b9b88bf75c7336b480ff3b48a42696
Adam Clemons <adamlouisclemons@gmail.com>
2021-02-06 19:50:46 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,17 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(num)
+  if num % 3 == 0 && num % 5 == 0
+    return "FizzBuzz"
+
+  else if num % 3 == 0
+    return "Fizz"
+
+  else if num % 5 == 0
+    return "Buzz"
+
+  else
+    return "nil"
+
+  end
+end
